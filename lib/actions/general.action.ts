250bcb8abd21de6df668fe1fd4c661cbb0a2d4ae
Atharva Patil <atharvapatil249@gmail.com>
2026-01-20 23:16:43 +0530

    implement interview generation , dynamic one

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -4,7 +4,7 @@ import { generateObject } from "ai";
 import { google } from "@ai-sdk/google";
 
 import { db } from "@/firebase/admin";
-import { feedbackSchema } from "@/constants";
+//import { feedbackSchema } from "@/constants";
 
 export async function createFeedback(params: CreateFeedbackParams) {
   const { interviewId, userId, transcript, feedbackId } = params;
@@ -20,7 +20,7 @@ export async function createFeedback(params: CreateFeedbackParams) {
     const { object } = await generateObject({
       model: google("gemini-2.0-flash-001", {
         structuredOutputs: false,
-      }),
+      }), 
       schema: feedbackSchema,
       prompt: `
         You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
